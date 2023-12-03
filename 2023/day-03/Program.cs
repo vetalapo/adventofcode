99d8a4acec93fb1b27ca2a5be2805db198b81090
Vitaly Dushkin <vetalapo@gmail.com>
2023-12-03 11:43:04 +0100

    Init for the day 3

---
+++ 2023/day-03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace AdventOfCode;
+
+public class Program
+{
+    public static void Main( string[] args )
+    {
+        if ( args.Length == 0 )
+        {
+            throw new ArgumentOutOfRangeException( "No aruments provided" );
+        }
+
+        string inputFile = args[0];
+
+        if ( !File.Exists( inputFile ) )
+        {
+            throw new FileNotFoundException( $"Cannot locate [{inputFile}]" );
+        }
+
+        WriteLine( $"Hello day 3!\r\nInput: {inputFile}" );
+
+        foreach( string line in File.ReadLines( inputFile ) )
+        {
+            WriteLine( line );
+        }
+    }
+}
---
+++ 2023/day-03/build-and-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dotnetVersion=$(dotnet --version)
+
+if [ $? -ne 0 ]; then
+    echo "Dotnet is not installed on your System"
+    exit 1
+fi
+
+echo "Building ad running the project..."
+echo
+
+dotnet run input.txt --configuration Release
