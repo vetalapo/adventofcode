3fc9fac76f6e4e9432edaa461f61bb7114b2cacd
Vitaly Dushkin <vetalapo@gmail.com>
2023-12-11 03:46:50 +0100

    Day 10, hello world

---
+++ 2023/day-10/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+namespace AdventOfCode;
+
+public class Program
+{
+    public static void Main( string[] args )
+    {
+        string inputFilePath = "input.txt";
+
+        if ( args.Length > 0 )
+        {
+            inputFilePath = args[0];
+        }
+
+        if ( !File.Exists( inputFilePath ) )
+        {
+            throw new FileNotFoundException( $"File not found at: {inputFilePath}" );
+        }
+
+        WriteLine( $"Hello, day 10! Input file: {inputFilePath}" );
+    }
+}
